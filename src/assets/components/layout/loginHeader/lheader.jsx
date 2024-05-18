7c69611c8cc1232985aa38f8fcbfdce3cbd62882
Marvin Ernesto <marvin.lagas01234@gmail.com>
2024-05-18 10:57:41 +0200

    Commit

---
+++ src/assets/components/layout/loginHeader/lheader.jsx
@@ -0,0 +1,8 @@
+import styles from './lheader.module.css'
+export default function LoginHeader() {
+  return (
+    <div className={styles.login_header}>
+      <h1>Biscateiro</h1>
+    </div>
+  );
+}
--- src/assets/components/login/login.jsx
+++ src/assets/components/login/login.jsx
@@ -1,24 +1,49 @@
 import styles from "./login.module.css";
+import LoginImg from "../../images/logimg.png";
+import LoginHeader from "../layout/loginHeader/lheader";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate("/bisc-sloder");
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.login_header}>
-        <h1>Biscateiro</h1>
-      </div>
-      <div>
-        <div>
-          <h2>Login</h2>
+    <div className={styles.main_container}>
+      <LoginHeader />
+      {/* Main Card que contém o formúlario e a imagem */}
+      <div className={styles.form_container}>
+        {/*Contém o formúlario */}
+        <div className={styles.formulario}>
+          <div className={styles.h2}>
+            <h2>Login</h2>
+          </div>
+          <div className={styles.form_content}>
+            <form action="#" className={styles.form}>
+              <div className={styles.input}>
+                <input
+                  type="text"
+                  placeholder="Nome, e-mail ou Número de telefone"
+                />
+              </div>
+              <div className={styles.input}>
+                <input type="password" placeholder="Password" />
+              </div>
+              <div className={styles.btn}>
+                <button id={styles.forgot}>Forgot password?</button>
+                <br />
+                <button id={styles.login} onClick={goBack}>Login</button>
+                <button id={styles.signup}>Or Signin</button>
+              </div>
+            </form>
+          </div>
         </div>
-        <form action="#">
-          <div className={styles.input_name}>
-            <label htmlFor="">Username or e-mail:</label>
-            <input type="text" minLength="8" maxLength="20" />
+        {/*Contém a imagem à direita */}
+        <div className={styles.img_container}>
+          <div className={styles.bg_green}>
+            <img src={LoginImg} alt="WIMIN" />
           </div>
-          <div className={styles.input_passwd}>
-            <label htmlFor="">Password:</label>
-            <input type="password" minLength='8'/>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
